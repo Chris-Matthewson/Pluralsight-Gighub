ef81dbbc381ce46a7955a3335d63071a9369cb2a
Chris Matthewson <cmatthewson2133@gmail.com>
2017-04-06 11:40:42 -0400

    implemented following

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,42 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var currentUserId = User.Identity.GetUserId();
+
+
+            if (_context.Attendances.Any(a => a.AttendeeId == currentUserId && a.GigId == dto.GigId))
+            {
+                return BadRequest("The attendance already exists.");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = currentUserId
+
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
