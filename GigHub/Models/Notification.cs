913b098d6b94686cd001ffb0265536681b3e7077
Chris <Chris@CHRISWRKTOWER>
2017-05-17 17:14:14 -0400

    added notification stuff to DB

--- GigHub/Models/Notification.cs
+++ GigHub/Models/Notification.cs
@@ -5,13 +5,30 @@ namespace GigHub.Models
 {
     public class Notification
     {
-        public int Id { get; set; }
-        public DateTime DateTime { get; set; }
-        public NotificationType Type { get; set; }
+        public int Id { get; private set; }
+        public DateTime DateTime { get; }
+        public NotificationType Type { get; }
         public DateTime? OriginalDateTime { get; set; }
         public string OriginalVenu { get; set; }
 
         [Required]
         public Gig Gig { get; set; }
+
+        protected Notification()
+        {
+            
+        }
+
+        public Notification(Gig gig, NotificationType type)
+        {
+            if (gig == null)
+            {
+                throw new ArgumentNullException(nameof(gig));
+            }
+
+            Gig = gig;
+            Type = type;
+            DateTime = DateTime.Now;
+        }
     }
 }
--- GigHub/Models/UserNotification.cs
+++ GigHub/Models/UserNotification.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -8,15 +9,34 @@ namespace GigHub.Models
     {
         [Key]
         [Column(Order = 1)]
-        public string UserId { get; set; }
+        public string UserId { get; }
         [Key]
         [Column(Order = 2)]
-        public int NotificationId { get; set; }
+        public int NotificationId { get; }
 
         public ApplicationUser User { get; set; }
 
-        public Notification Notification { get; set; }
+        public Notification Notification { get; }
 
         public bool IsRead { get; set; }
+
+        protected UserNotification()
+        {
+            
+        }
+
+        public UserNotification(ApplicationUser user, Notification notification)
+        {
+            if (User == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            if (Notification == null)
+            {
+                throw new ArgumentNullException(nameof(notification));
+            }
+            User = user;
+            Notification = notification;
+        }
     }
 }
