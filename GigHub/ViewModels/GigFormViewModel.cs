10933abfef1c754f8dfe233ea685866f3ab1eb19
Chris <Chris@CHRISWRKTOWER>
2017-05-17 13:23:44 -0400

    -edit gigs

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -2,11 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using GigHub.Controllers;
+using System.Linq.Expressions;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        public int Id { get; set; }
         [Required]
         public string Venue { get; set; }
 
@@ -24,5 +28,19 @@ namespace GigHub.ViewModels
         public IEnumerable<Genre> Genres { get; set; }
 
         public DateTime GetDateTime() => DateTime.Parse(string.Format($"{Date} {Time}"));
+
+        public string Heading { get; set; }
+
+        public string Action
+        {
+            get
+            {
+                Expression<Func<GigsController, ActionResult>> update = 
+                    (c => c.Update(null));
+                Expression<Func<GigsController, ActionResult>> create = 
+                    (c => c.Create(null));
+                return (Id != 0) ? update.Name : create.Name;
+            }
+        }
     }
 }
